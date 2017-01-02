6255ec8b01d3e60bce2fd4ef92dd2b0dbc391a5c
BrianKoss951001 <tendor292527@gmail.com>
2017-01-02 13:46:38 +1300

    lint

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Section1 = (props/* , context*/) => {
+const Section1 = (props) => {
   const {
     arrIndex, data, edit, onChange,
     onClickSave, onClickDelete,
@@ -13,7 +13,7 @@ const Section1 = (props/* , context*/) => {
         <h2>{data.subTitle}</h2>
         <img src={data.linkImage} role="presentation" />
         <div className="FormActions">
-          <button type="button" onClick={onClickSave.bind(null, arrIndex)}>Edit</button>
+          <button type="button" onClick={e => onClickSave(arrIndex, e)}>Edit</button>
         </div>
       </div>
     )
@@ -23,19 +23,19 @@ const Section1 = (props/* , context*/) => {
     <div>
       <label className="FormGroup" htmlFor="Sec1-title">
         <span>Title</span>
-        <input className="FormControl" name="title" id="Sec1-title" type="text" value={data.title} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="title" id="Sec1-title" type="text" value={data.title} onChange={e => onChange(arrIndex, e)} />
       </label>
       <label className="FormGroup" htmlFor="Sec1-subTitle">
         <span>Sub Title</span>
-        <input className="FormControl" name="subTitle" id="Sec1-subTitle" type="text" value={data.subTitle} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="subTitle" id="Sec1-subTitle" type="text" value={data.subTitle} onChange={e => onChange(arrIndex, e)} />
       </label>
       <label className="FormGroup" htmlFor="Sec1-linkImage">
         <span>Link Image</span>
-        <input className="FormControl" name="linkImage" id="Sec1-linkImage" type="text" value={data.linkImage} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="linkImage" id="Sec1-linkImage" type="text" value={data.linkImage} onChange={e => onChange(arrIndex, e)} />
       </label>
       <div className="FormActions">
-        <button type="button" onClick={onClickSave.bind(null, arrIndex)}>Save</button>
-        <button type="button" onClick={onClickDelete.bind(null, arrIndex)}>Delete</button>
+        <button type="button" onClick={e => onClickSave(arrIndex, e)}>Save</button>
+        <button type="button" onClick={e => onClickDelete(arrIndex, e)}>Delete</button>
       </div>
     </div>
   )
