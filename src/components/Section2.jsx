2df5291c5bec1fb020cf95ea591ab49b89940e74
BrianKoss951001 <tendor292527@gmail.com>
2017-01-03 13:46:45 +1300

    lint

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Section2 = (props, context) => {
+const Section2 = (props) => {
   const {
     arrIndex, data, edit, onChange,
     onClickSave, onClickDelete,
@@ -13,14 +13,14 @@ const Section2 = (props, context) => {
         <h2>{data.subTitle}</h2>
         <a href={data.buttonLink} target={data.buttonTarget}>{data.buttonLabel}</a>
         <div className="FormActions">
-          <button type="button" onClick={onClickSave.bind(null, arrIndex)}>Edit</button>
+          <button type="button" onClick={e => onClickSave(arrIndex, e)}>Edit</button>
         </div>
       </div>
     )
   }
 
   const buttonTargetOptions = (
-    <select className="FormControl" name="buttonTarget" id="Sec2-buttonTarget" value={data.buttonTarget} onChange={onChange.bind(null, arrIndex)}>
+    <select className="FormControl" name="buttonTarget" id="Sec2-buttonTarget" value={data.buttonTarget} onChange={e => onChange(arrIndex, e)}>
       <option value="_self">Self</option>
       <option value="_blank">Blank</option>
       <option value="_parent">Parent</option>
@@ -32,19 +32,19 @@ const Section2 = (props, context) => {
     <div>
       <label className="FormGroup" htmlFor="Sec2-title">
         <span>Title</span>
-        <input className="FormControl" name="title" id="Sec2-title" type="text" value={data.title} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="title" id="Sec2-title" type="text" value={data.title} onChange={e => onChange(arrIndex, e)} />
       </label>
       <label className="FormGroup" htmlFor="Sec2-subTitle">
         <span>Sub Title</span>
-        <input className="FormControl" name="subTitle" id="Sec2-subTitle" type="text" value={data.subTitle} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="subTitle" id="Sec2-subTitle" type="text" value={data.subTitle} onChange={e => onChange(arrIndex, e)} />
       </label>
       <label className="FormGroup" htmlFor="Sec2-backgroundColor">
         <span>Background Color</span>
-        <input className="FormControl" name="backgroundColor" id="Sec2-backgroundColor" type="text" value={data.backgroundColor} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="backgroundColor" id="Sec2-backgroundColor" type="text" value={data.backgroundColor} onChange={e => onChange(arrIndex, e)} />
       </label>
       <label className="FormGroup" htmlFor="Sec2-buttonLink">
         <span>Button - Link</span>
-        <input className="FormControl" name="buttonLink" id="Sec2-buttonLink" type="text" value={data.buttonLink} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="buttonLink" id="Sec2-buttonLink" type="text" value={data.buttonLink} onChange={e => onChange(arrIndex, e)} />
       </label>
       <label className="FormGroup" htmlFor="Sec2-buttonTarget">
         <span>Buton - Target</span>
@@ -52,11 +52,11 @@ const Section2 = (props, context) => {
       </label>
       <label className="FormGroup" htmlFor="Sec2-buttonLabel">
         <span>Buton - Label</span>
-        <input className="FormControl" name="buttonLabel" id="Sec2-buttonLabel" type="text" value={data.buttonLabel} onChange={onChange.bind(null, arrIndex)} />
+        <input className="FormControl" name="buttonLabel" id="Sec2-buttonLabel" type="text" value={data.buttonLabel} onChange={e => onChange(arrIndex, e)} />
       </label>
       <div className="FormActions">
-        <button type="button" onClick={onClickSave.bind(null, arrIndex)}>Save</button>
-        <button type="button" onClick={onClickDelete.bind(null, arrIndex)}>Delete</button>
+        <button type="button" onClick={e => onClickSave(arrIndex, e)}>Save</button>
+        <button type="button" onClick={e => onClickDelete(arrIndex, e)}>Delete</button>
       </div>
     </div>
   )
