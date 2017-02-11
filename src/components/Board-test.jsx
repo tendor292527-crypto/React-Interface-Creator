e205b51185252fd49223a4033e212cd2207f0c4d
BrianKoss951001 <tendor292527@gmail.com>
2017-02-12 10:18:35 +1300

    updating Board test to use DnD helpers

--- src/components/Board-test.jsx
+++ src/components/Board-test.jsx
@@ -3,39 +3,42 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
 
-import Constructor from './Constructor'
 import Board from './Board'
 
 describe('<Board />', () => {
   it('renders correctly', () => {
+    const OriginalBoard = Board.DecoratedComponent
+    const identity = el => el
     const wrapper = shallow(
-      <Constructor>
-        <Board />
-      </Constructor>,
+      <OriginalBoard connectDropTarget={identity} drop>
+        {[]}
+      </OriginalBoard>,
     )
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
   it('renders with one child correctly', () => {
+    const OriginalBoard = Board.DecoratedComponent
+    const identity = el => el
     const wrapper = shallow(
-      <Constructor>
-        <Board>
-          <span>Fake</span>
-        </Board>
-      </Constructor>,
+      <OriginalBoard connectDropTarget={identity} drop>
+        {[<span key="1">Fake</span>]}
+      </OriginalBoard>,
     )
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
   it('renders with multiple child correctly', () => {
+    const OriginalBoard = Board.DecoratedComponent
+    const identity = el => el
     const wrapper = shallow(
-      <Constructor>
-        <Board>
-          <span>Fake</span>
-          <span>Fake</span>
-          <span>Fake</span>
-        </Board>
-      </Constructor>,
+      <OriginalBoard connectDropTarget={identity} drop>
+        {[
+          <span key="1">Fake</span>,
+          <span key="2">Fake</span>,
+          <span key="3">Fake</span>,
+        ]}
+      </OriginalBoard>,
     )
     expect(toJson(wrapper)).toMatchSnapshot()
   })
