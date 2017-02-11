7b33bea06ff05a3f16942f7824f0249e83c216b0
BrianKoss951001 <tendor292527@gmail.com>
2017-02-12 10:27:47 +1300

    jest env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,20 @@
 module.exports = {
-    root: true,
-    parser: 'babel-eslint',
-    extends: ['airbnb'],
-    globals: {
-        expect: true,
-        createSpy: true,
-        spyOn: true,
-        isSpy: true,
-    },
-    parseOptions: {
-        ecmaVersion: 6,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    env: {
-        browser: true,
-        es6: true,
-        mocha: true
-    },
-    rules: {
-        semi: [2, "never"]
+  root: true,
+  parser: 'babel-eslint',
+  extends: ['airbnb'],
+  parseOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
     }
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  rules: {
+    semi: [2, "never"]
+  }
 }
