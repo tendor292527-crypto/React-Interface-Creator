e75304eded83d2b035698057f1f633d1f4224991
BrianKoss951001 <tendor292527@gmail.com>
2017-03-17 18:34:59 +1300

    moaaar tests

--- src/stores/SectionsStore-test.js
+++ src/stores/SectionsStore-test.js
@@ -5,4 +5,22 @@ describe('SectionsStore', () => {
     const actual = SectionsStore.get('section1')
     expect(actual.type).toBe('1')
   })
+
+  it('retursn Section 2 correctly', () => {
+    const actual = SectionsStore.get('section2')
+    expect(actual.type).toBe('2')
+  })
+
+  it('retursn Section 3 correctly', () => {
+    const actual = SectionsStore.get('section3')
+    expect(actual.type).toBe('3')
+  })
+
+  it('retursn correct error', () => {
+    try {
+      SectionsStore.get('section4')
+    } catch (err) {
+      expect(err).toBeDefined()
+    }
+  })
 })
